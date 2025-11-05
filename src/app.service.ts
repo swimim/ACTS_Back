1b6201e98e1a30b935b21c0ee1a8382544d61708
HyunWoo <162387986+acorn497@users.noreply.github.com>
2025-11-05 18:17:11 +0900

    Feat: 서비스 정보 반환 API

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { uptime } from 'process';
+import pkg from "../package.json";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getServerInformation() {
+    let runningtime: number | string = Math.round(uptime());
+    runningtime = ((runningtime < 60)
+      ? runningtime + " Seconds"
+      : ((runningtime >= 60 && runningtime < 3600)
+        ? (Math.floor((runningtime / 60) * 100) / 100) + " Minutes"    // 분
+        : (Math.floor((runningtime / 3600) * 100) / 100) + " Hours")); // 시
+
+    return {
+      status: "Running",
+      timestamp: new Date(),
+      uptime: runningtime,
+      version: pkg.version,
+      description: pkg.description,
+    };
   }
 }
