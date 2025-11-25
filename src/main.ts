de38e9e8ceab30768a7e8a9e65fa84fc7ed401cc
swimim <rnjstndud6500@naver.com>
2025-11-26 01:45:29 +0900

    chore: CORS 중복 제거

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
-
   app.useGlobalPipes(new ValidationPipe);
   app.use(cookieParser());
   await app.listen(process.env.SERVER_PORT ?? 3000);
